02a2d8e5d4cd7d8b7cd1091e25a3af1d5fc40dcc
Anirudh Patel <patel.anirudh0025@gmail.com>
2022-03-12 18:42:43 -0700

    Partial commit. Need to refactor change

--- python/vending_machine/test_vending_machine.py
+++ python/vending_machine/test_vending_machine.py
@@ -2,6 +2,7 @@
 
 import vending_machine as vm
 
+
 def test_update_inventory_sold_out():
     inventory_0 = vm.Inventory(soda=0, chips=1, candy=2)
 
@@ -16,6 +17,7 @@ def test_update_inventory_sold_out():
     assert inventory_1.chips == 1
     assert inventory_1.candy == 2
     assert display == "SOLD OUT"
+
 
 def test_update_inventory_decrement():
     inventory_0 = vm.Inventory(soda=0, chips=1, candy=2)
@@ -43,3 +45,37 @@ def test_dime():
     w = 1010.0
     assert vm.coin_value(weight=w, size=s) == 0.10
 
+
+def test_nickel_change():
+    # Simple case
+    amount_paid = 1.05
+    price = 1.0
+    bank = vm.Bank(nickels=2, quarters=1, dimes=10)
+    c0 = vm.change(amount_paid=amount_paid, bank=bank, price=price)
+    expected = (vm.Coin(size=5.0, weight=55.0, value=0.05),)
+    assert all([c == e for c, e in zip(c0, expected)])
+    assert len(expected) == len(c0)
+
+
+def test_out_of_change():
+    # No change
+    amount_paid = 1.05
+    price = 1.0
+    bank = vm.Bank(nickels=0, quarters=1, dimes=10)
+    c0 = vm.change(amount_paid=amount_paid, bank=bank, price=price)
+    expected = tuple()
+    assert len(expected) == len(c0)
+
+
+def test_nickel_quarter_change():
+    # Multiple coins as output
+    amount_paid = 1.30
+    price = 1.0
+    bank = vm.Bank(nickels=2, quarters=1, dimes=10)
+    c0 = vm.change(amount_paid=amount_paid, bank=bank, price=price)
+    expected = (
+        vm.Coin(size=5.0, weight=55.0, value=0.05),
+        vm.Coin(size=25.0, weight=2525.0, value=0.25),
+    )
+    assert all([c == e for c, e in zip(c0, expected)])
+    assert len(expected) == len(c0)
--- python/vending_machine/vending_machine.py
+++ python/vending_machine/vending_machine.py
@@ -1,4 +1,6 @@
 import functools
+import itertools
+import math
 from typing import Literal, NamedTuple
 
 
@@ -13,20 +15,38 @@ class Coin(NamedTuple):
     weight: float
     value: float
 
+
+class Bank(NamedTuple):
+    nickels: int
+    dimes: int
+    quarters: int
+
+
 Display = Literal["SOLD OUT", "INSERT COIN"]
+
+
+def quarter() -> Coin:
+    return Coin(size=25.0, weight=2525.0, value=0.25)
+
+
+def dime() -> Coin:
+    return Coin(size=10.0, weight=1010.0, value=0.10)
+
+
+def nickel() -> Coin:
+    return Coin(size=5.0, weight=55.0, value=0.05)
 
 
 def valid_coins() -> tuple[Coin, Coin, Coin]:
 
-    nickel = Coin(size=5.0, weight=55.0, value=0.05)
-    dime = Coin(size=10.0, weight=1010.0, value=0.10)
-    quarter = Coin(size=25.0, weight=2525.0, value=0.25)
+    ni = nickel()
+    di = dime()
+    qu = quarter()
 
-    return (nickel, dime, quarter)
+    return (ni, di, qu)
 
 
 def coin_value(*, size: float, weight: float) -> float:
-
     def conditional_value(size, weight, coin) -> float:
         value = 0.0  # default value
         if size == coin.size and weight == coin.weight:
@@ -45,7 +65,6 @@ def update_inventory(
     selected_candy: bool,
     selected_chips: bool
 ) -> tuple[Inventory, Display]:
-
     def update_item(qty: int, dqty: int) -> tuple[int, bool]:
         new_qty = qty - dqty
         return new_qty, new_qty >= 0
@@ -58,11 +77,25 @@ def update_inventory(
     display: Display = "SOLD OUT"
 
     if all((soda_ok, chips_ok, candy_ok)):
-        updated = Inventory(
-            soda=usoda,
-            chips=uchips,
-            candy=ucandy,
-        )
+        updated = Inventory(soda=usoda, chips=uchips, candy=ucandy,)
         display = "INSERT COIN"
 
     return updated, display
+
+
+def change(*, amount_paid: float, bank: Bank, price: float) -> tuple[Coin, ...]:
+    overage = round(amount_paid - price, 2)
+
+    num_quarters = min(math.floor(overage / 0.25), bank.quarters)
+    overage = round(overage - (num_quarters * 0.25), 2)
+    num_dimes = min(math.floor(overage / 0.1), bank.dimes)
+    overage = round(overage - (num_dimes * 0.1), 2)
+    num_nickels = min(math.floor(overage / 0.05), bank.nickels)
+
+    return tuple(
+        itertools.chain(
+            itertools.repeat(nickel(), num_nickels),
+            itertools.repeat(dime(), num_dimes),
+            itertools.repeat(quarter(), num_quarters),
+        )
+    )
