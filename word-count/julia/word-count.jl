eedd21fba4c14fcd87a36ab3d4109a57235a03cd
Chad Hovey <chovey@sandia.gov>
2022-09-08 09:56:02 -0600

    incremental

--- word-count/julia/word-count.jl
+++ word-count/julia/word-count.jl
@@ -1,5 +1,14 @@
-# function wordcount(sentence::AbstractString)
-function ff(sentence::AbstractString)
-    aa = strip(sentence)
-    bb = split(aa, isspace, keepempty=false)
+function wordcount(sentence::AbstractString)
+# function ff(sentence::AbstractString)
+  println("input: $sentence")
+  aa = strip(lowercase(sentence))
+  bb = split(aa, isspace, keepempty=false)
+  println("bb: $bb")
+  removals = ['\"', '\'', '!', ':', '.', ',']
+  # return cc = [lstrip(x, ['\'', '!']) for x in bb]
+  cc = [lstrip(x, removals) for x in bb]
+  dd = [rstrip(x, removals) for x in cc]
+  ddset = Set(dd)
+  counts = [count(x == y for x in dd) for y in ddset]
+  results = Dict(zip(ddset, counts))
 end
