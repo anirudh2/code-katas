b5386cd9eaf17ea14cbf3e5ec3c496686048f6c6
Chad Hovey <hovey@me.com>
2022-09-09 14:40:50 -0600

    pause word count for now...

--- word-count/julia/word-count.jl
+++ word-count/julia/word-count.jl
@@ -1,16 +1,27 @@
 sentence = " .\n,\t!^&*()~@#\$%{}[]:;'/<>"
 
 function wordcount(sentence::AbstractString)
+  # isalpha(x) = issubset(x, 'a':'z')
+  # isalpha_num(x) = issubset(x, 'a':'z') || issubset(x, 0:9)
+  isalpha_num(x) = issubset(x, 'a':'z') || issubset(parse(Int, x), 0:9)
+  isspace_ext(x) = isspace(x) || (x == ',')
 
-  println("input: $sentence")
+  removals = ['\"', '\'', '\`', '?', '!', ':', '.', ',', '&', '@', '\\', '$', '%', '^']
+
+  println("\ninput: $sentence")
   aa = strip(lowercase(sentence))
-  bb = split(aa, isspace, keepempty=false)
-  isalpha(x) = issubset(x, 'a':'z')
-  # bb = [filter(isalpha, bb)]
-  removals = ['\"', '\'', '\`', '?', '!', ':', '.', ',']
+  # bb = split(aa, isspace, keepempty=false)
+  # bb1 = split(aa, isspace, keepempty=false)
+  bb = split(aa, isspace_ext, keepempty=false)
+  #
+  # cc = [rstrip(x, removals) for x in bb]
+  # bb2 = split(bb1, ',', keepempty=false)
+  # cc = [rstrip(x, removals) for x in bb2]
   cc = [rstrip(x, removals) for x in bb]
   dd = [lstrip(x, removals) for x in cc]
-  ddset = Set(filter(isalpha, dd))
+  #
+  # ddset = Set(filter(isalpha, dd))
+  ddset = Set(filter(isalpha_num, dd))
   ee = filter(!isempty, ddset)
   # counts = [count(x == y for x in dd) for y in ddset]
   counts = [count(x == y for x in dd) for y in ee]
