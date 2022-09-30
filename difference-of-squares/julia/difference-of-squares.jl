74b47b09a6eb0d4e5dd9e95de15f8f1c660f8ae9
Chad Hovey <chovey@sandia.gov>
2022-09-30 17:20:16 -0600

    cbh diff of squares soln

--- difference-of-squares/julia/difference-of-squares.jl
+++ difference-of-squares/julia/difference-of-squares.jl
@@ -1,14 +1,14 @@
 "Square the sum of the first `n` positive integers"
 function square_of_sum(n)
-
+    sum(range(1, step=1, stop=n))^2
 end
 
 "Sum the squares of the first `n` positive integers"
 function sum_of_squares(n)
-
+    sum([x^2 for x in range(1, step=1, stop=n)])
 end
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
 function difference(n)
-
+    square_of_sum(n) - sum_of_squares(n)
 end
