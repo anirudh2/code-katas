0d4649017b218e553f5d9b24abe4bb466b7dec4b
Chad Hovey <chovey@sandia.gov>
2022-09-30 17:23:35 -0600

    update from code kata on conference travel

--- nucleotide-count/julia/nucleotide-count.jl
+++ nucleotide-count/julia/nucleotide-count.jl
@@ -6,6 +6,7 @@ The count of each nucleotide within `strand` as a dictionary.
 Invalid strands raise a `DomainError`.
 
 """
-function count_nucleotides(strand)
-
+function count_nucleotides(strand::AbstractString)
+    iter = ['A', 'C', 'G', 'T']
+    counts = [count(x, uppercase("asdf")) for x in ['A', 'C', 'G', 'T']]
 end
