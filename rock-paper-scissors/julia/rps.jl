e04a6e4b6db243a98e62a22ecc6848c4f10fb94f
Chad B. Hovey <hovey@me.com>
2022-12-08 09:20:56 -0700

    incremental backup

--- rock-paper-scissors/julia/rps.jl
+++ rock-paper-scissors/julia/rps.jl
@@ -4,17 +4,17 @@ const scissors_sym = Symbol("scissors")
 
 abstract type Hand end
 
-struct Rock
+struct Rock <: Hand
     name::String
     Rock() = new("rock")
 end
 
-struct Paper
+struct Paper <: Hand
     name::String
     Paper() = new("paper")
 end
 
-struct Scissors
+struct Scissors <: Hand
     name::String
     Scissors() = new("scissors")
 end
@@ -23,21 +23,44 @@ r = Rock()
 p = Paper()
 s = Scissors()
 
-# actions = ("rock", "paper", "scissors")
-actions = [i.name for i in [r, p, s]]
 
-struct Action
-    name::String
-    hand::Hand
-    # name::String
-    # Action(::Val{rock_sym}) = new(Rock())
-    # Action(::Val{paper_sym}) = new(Paper())
-    # Action(::Val{scissors_sym}) = new(Scissors())
-    Action(name) = new(Rock())
-        if name == "rock" 
-    elseif name == "paper"
-        Action(name) = n
+function HandFactory(type::String)::Hand
+    if type == "rock"
+        return Rock()
+    elseif type == "paper"
+        return Paper()
+    elseif type == "scissors"
+        return Scissors()
+    end
 end
+
+hand(a::Player) = a.hand.name
+
+# actions = ("rock", "paper", "scissors")
+# actions = [i.name for i in [r, p, s]]
+hands = [i.name for i in [r, p, s]]
+
+struct Player
+    # hand::String
+    # Player(hand) = new(hand)
+
+    hand::Hand
+    Player(name::String) = new(HandFactory(name))
+end
+
+
+# struct Action
+#     name::String
+#     hand::Hand
+#     # name::String
+#     # Action(::Val{rock_sym}) = new(Rock())
+#     # Action(::Val{paper_sym}) = new(Paper())
+#     # Action(::Val{scissors_sym}) = new(Scissors())
+#     Action(name) = new(Rock())
+#         if name == "rock" 
+#     elseif name == "paper"
+#         Action(name) = n
+# end
 
 # name(a::Action) = a.name # accessor
 
@@ -51,18 +74,21 @@ end
 function main()::Cint
 
     while true
-        print("Enter a choice $actions: ")
+        print("Enter a choice $hands: ")
         # user_action = readline()
         # user_action = Action(readline())
         # user_action = Action(Symbol(readline()))
-        user_action = Action(Val(Symbol(readline())))
+        # user_action = Action(Val(Symbol(readline())))
+        human = Player(readline())
 
         # computer_action = rand(actions)
         # computer_action = Action(rand(actions))
-        computer_action = Action(Val(Symbol(rand(actions))))
+        # computer_action = Action(Val(Symbol(rand(actions))))
+        computer = Player(rand(hands))
 
         # println("\nYou chose $user_action, computer chose $computer_action.\n")
-        println("\nYou chose $name(user_action), computer chose $name(computer_action).\n")
+        # println("\nYou chose $name(user_action), computer chose $name(computer_action).\n")
+        println("\nYou chose " * hand(human) * ", computer chose " * hand(computer) * ".\n")
 
         print("Play again? (y/n): ")
         play_again = readline()
